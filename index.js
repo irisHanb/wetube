bdc14b19c5c6ca7d652246c85a7f21646a1bd059
irisHanb <fortrulymylife@gmail.com>
2020-11-27 14:47:28 +0900

    setting middlerwares
    helmet, morgan, cookie-parser, body-parser

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import bodyparser from 'body-parser';
 
 const app = express();
 const PORT = 4000;
@@ -9,6 +13,12 @@ const handleListening = () =>
 const handleHome = (req, res) => res.send('hello');
 
 const handleProfile = (req, res) => res.send('hello my profile');
+
+app.use(cookieParser());
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan('dev'));
 
 app.get('/', handleHome);
 app.get('/profile', handleProfile);
