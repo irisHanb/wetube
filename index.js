bc9ec6a7aa55b3e68acb0ba312d28c9e0d105545
irisHanb <fortrulymylife@gmail.com>
2020-11-27 13:15:41 +0900

    서버 기본 테스트

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('hi');
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+function handleListening(req, res) {
+  console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+function handleHome(req, res) {
+  res.send('hello');
+}
+function handleProfile(req, res) {
+  res.send('hello my profile');
+}
+
+app.get('/', handleHome);
+app.get('/profile', handleProfile);
+
+app.listen(PORT, handleListening);
