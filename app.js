a7e0eaf5e67b11da618e491850cf9a9e44b6e7ea
irisHanb <fortrulymylife@gmail.com>
2020-11-27 15:41:40 +0900

    add routers

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyparser from 'body-parser';
-import { userRouter } from './router';
+import globalRouter from './routers/globalRouter';
+import userRouter from './routers/userRouter';
+import videoRouter from './routers/videoRouter';
 
 const app = express();
-
-const handleHome = (req, res) => res.send('hello');
-
-const handleProfile = (req, res) => res.send('hello my profile');
 
 app.use(cookieParser());
 app.use(bodyparser.json());
@@ -17,8 +15,8 @@ app.use(bodyparser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan('dev'));
 
-app.get('/', handleHome);
-app.get('/profile', handleProfile);
-app.use('/user', userRouter);
+app.use('/', globalRouter);
+app.use('/users', userRouter);
+app.use('/videos', videoRouter);
 
 export default app;
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,7 +1,9 @@
 import express from 'express';
 
-export const userRouter = express.Router();
+const userRouter = express.Router();
 
 userRouter.get('/', (req, res) => res.send('user'));
 userRouter.get('/profile', (req, res) => res.send('user profile'));
 userRouter.get('/password', (req, res) => res.send('user password'));
+
+export default userRouter;
